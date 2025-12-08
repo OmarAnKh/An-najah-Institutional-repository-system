bac9aeb496f01e4955aee28af5d6121110c96866
aboodspammm <8ghostin@gmail.com>
2025-12-08 18:09:52 +0200

    add doc strings to ever class/method i implemented

--- extracters/stanza_locations_extractor.py
+++ extracters/stanza_locations_extractor.py
@@ -2,7 +2,21 @@ from extracters.abc_extractor import ABCExtractor
 from models.stanza_models import get_model
 
 class StanzaLocationsExtractor(ABCExtractor):
+    """
+    Extracts location names from text using Stanza NLP library.
+    Locations include geopolitical entities, facilities, and organizations.
+    """
     def extract(self, text, lang = 'en'):
+        """
+        extract location names from a given text.
+
+        Args:
+            text (str): The input text from which to extract location names.
+            lang (str, optional): The language of the input text. Defaults to 'en'.
+
+        Returns:
+            set: A set of extracted location names.
+        """
         self._validate_text(text)
         nlp = get_model(lang)
         doc = nlp(text)
--- models/stanza_models.py
+++ models/stanza_models.py
@@ -1,13 +1,28 @@
 import stanza
 
+# Dictionary to hold loaded models
+# Key: language code, Value: Stanza NLP pipeline
 _models = {}
-_langugages = ['en', 'ar']
+# Supported languages
+_languages = ['en', 'ar']
 
-for lang_code in _langugages:
+# Download models for supported languages
+for lang_code in _languages:
     stanza.download(lang_code)
     
 def get_model(lang):
-    if lang not in _langugages:
+    """
+    Get the Stanza NLP model for the specified language.
+    Args:
+        lang (str): The language code for the desired model.
+        
+    Returns:
+        stanza.Pipeline: The Stanza NLP pipeline for the specified language.
+        
+    Raises:
+        ValueError: If the specified language is not supported.    
+    """
+    if lang not in _languages:
         raise ValueError(f"Language '{lang}' is not supported.")
     if lang not in _models:
         _models[lang] = stanza.Pipeline(lang=lang, processors='tokenize,mwt,ner')
